1c18e0444ac5d7f5004835f06268416491662bbb
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-03-13 19:53:31 -0400

    Leetcode isBadVersion
    Algorithm 
    -It's essentially lower_bound, 
    - Finding the smallest element that returns "true" to the function.
    - Problems like these are hard to spot whether it's binary search or not.

---
+++ isBadVersion.cpp
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 0;
+        int high = n;
+        int firstBad = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isBadVersion(mid)){
+                 firstBad = mid;
+                 high = mid-1;
+            }
+            else if(!isBadVersion(mid)){
+                low = mid +1;
+            }
+        }
+        return firstBad;
+           }
+};
