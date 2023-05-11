0538d422b03787933c92ca11e6a3f0a7c495b222
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-05-10 22:56:46 -0400

    Create circlecross.cpp
    http://www.usaco.org/index.php?page=viewproblem2&cpid=712

---
+++ circlecross.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <math.h>
+#include <stdio.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <list>
+#include <climits>
+using namespace std;
+#define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long int ll;
+
+// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+
+
+
+int main() {
+    FAST
+    int n = 52;
+    int cnt = 0;
+    vector<char> cows(n);
+    for(int i = 0; i <n;i++) cin>>cows[i];
+    for(int i =0; i <n;i++){
+        for(int j = i+1; j < n;j++){
+            for(int k = j+1; k<n;k++){
+                for(int l = k+1; l<n;l++){
+                    cnt += (cows[i] == cows[k] && cows[j] == cows[l]);
+                }
+            }
+        }
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
