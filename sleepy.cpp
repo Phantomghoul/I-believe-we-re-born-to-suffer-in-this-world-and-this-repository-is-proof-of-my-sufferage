ebca1e9d947769622f0d37b4fa3abe6ed3c688d9
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-05-25 21:09:31 -0400

    Create sleepy.cpp
    http://www.usaco.org/index.php?page=viewproblem2&cpid=892

---
+++ sleepy.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <math.h>
+#include <stdio.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <list>
+#include <climits>
+using namespace std;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long int ll;
+// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+
+
+
+int main() {
+    FAST
+    int n; cin>>n;
+    vector<int> a(n);
+    for(int i =0; i <n;i++)cin>>a[i];
+
+    int x = 1;
+    for(int i = n-2; i>=0 && a[i] < a[i+1];i--)x++;
+    cout<<n-x<<endl;
+
+}
