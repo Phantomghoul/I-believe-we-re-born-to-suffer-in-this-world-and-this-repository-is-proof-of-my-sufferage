fdb205de6e2446b8f59d470afcf3233be89cdb8c
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-05-28 18:14:02 -0400

    Create breedflip.cpp
    http://www.usaco.org/index.php?page=viewproblem2&cpid=1012

---
+++ breedflip.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <fstream>
+#include <math.h>
+#include <stdio.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <list>
+#include <climits>
+using namespace std;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long int ll;
+// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+
+int main()
+{
+    FAST
+    int n; cin>>n;
+    string a,b; cin>>a>>b;
+    int cnt = 0;
+    bool flag = false;
+    for(int i =0; i <n;i++){
+       if(a[i]!=b[i]){
+           if(!flag){
+               flag = true;
+               cnt++;
+           }
+       }
+       else{
+           flag = false;
+       }
+    }
+    cout<<cnt<<endl;
+
+}
+
+
