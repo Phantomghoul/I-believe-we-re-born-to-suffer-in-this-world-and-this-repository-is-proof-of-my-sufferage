f3b619207ad597cc1d00166ece3b64955bb01f51
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-06-02 19:15:48 -0400

    Create planting.cpp
    http://www.usaco.org/index.php?page=viewproblem2&cpid=894

---
+++ planting.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <cassert>
+#include <fstream>
+#include <math.h>
+#include <stdio.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <list>
+#include <climits>
+using namespace std;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long int ll;
+// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+
+int main(){
+    FAST
+    int n; cin>>n;
+    vector<int> degree(n);
+    for(int i = 1; i<n;i++){
+        int a,b; cin>>a>>b;
+        degree[a-1]++; degree[b-1]++;
+    }
+    int ans = 0;
+    for(int i =0; i <n;i++){
+        ans = max(ans, degree[i]);
+    }
+    cout<<ans +1<<endl;
+}
+
+
