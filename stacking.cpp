c5ef62f608a3ffeba60850b5852166cb644c342b
Phantomghoul <67940517+Phantomghoul@users.noreply.github.com>
2023-06-03 23:28:47 -0400

    Create stacking.cpp
    http://www.usaco.org/index.php?page=viewproblem2&cpid=104

---
+++ stacking.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <cassert>
+#include <fstream>
+#include <math.h>
+#include <stdio.h>
+#include <utility>
+#include <stack>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+#include <cstring>
+#include <queue>
+#include <cctype>
+#include <bits/stdc++.h>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <list>
+#include <climits>
+using namespace std;
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long int ll;
+// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+//        .==.        .==.
+//       //`^\\      //^`\\
+//      // ^ ^\(\__/)/^ ^^\\
+//     //^ ^^ ^/6  6\ ^^ ^ \\
+//    //^ ^^ ^/( .. )\^ ^ ^ \\
+//   // ^^ ^/\| v""v |/\^ ^ ^\\
+//  // ^^/\/ /  `~~`  \ \/\^ ^\\
+//  -----------------------------
+
+void setIO(string s){
+    FAST
+    freopen((s + ".in").c_str(), "r",stdin);
+    freopen((s+ ".out").c_str(),"w",stdout);
+}
+
+
+int main(){
+    int n,k; cin>>n>>k;
+    vector<int> diff(n+1);
+
+    for(int i =0; i <k;i++){
+        int l,r; cin>>l>>r;
+        l--; r--;
+        diff[l]++;
+        diff[r+1]--;
+    }
+
+    vector<int> sol(n);
+    int tot = 0;
+
+    for(int i =0; i <n;i++){
+        tot += diff[i];
+        sol[i] = tot;
+    }
+    sort(all(sol));
+    cout<<sol[n/2]<<endl;
+}
