e5108ce823b5a478f5385ffa8355454a379e6b68
Phantomghoul <mnden136@gmail.com>
2025-05-24 05:20:46 -0400

    1979C CF

---
+++ 1979C.cpp
@@ -0,0 +1,266 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <string.h>
+//#include <cassert>
+//#include <fstream>
+//#include <math.h>
+//#include <stdio.h>
+//#include <utility>
+//#include <stack>
+//#include <algorithm>
+//#include <map>
+//#include <iomanip>
+//#include <cstring>
+//#include <queue>
+//#include <cctype>
+//#include <bits/stdc++.h>
+//#include <fstream>
+//#include <unordered_map>
+//#include <unordered_set>
+//#include <cmath>
+//#include <list>
+//#include <climits>
+//
+//
+//
+//using namespace std;
+//#define endl "\n"
+//#define all(x) (x).begin(), (x).end()
+//#define FAST ios_base::sync_with_stdio(false); cin.tie(0);
+//#define sz(x) (int)x.size()
+//#define vi vector<int>
+//#define vs vector<string>
+//#define int long long
+//#define ll long long
+//#define pi pair<int,int>
+//#define pii pair<pair<int,int>,int>
+//#define in(a,b)  ( (b).find(a) != (b).end())
+//#define rep(i,a,b)  for(int i=a;i<b;i++)
+//#define trav(a, x) for (auto& a : x)
+//#define vll vector<ll>
+//#define pb push_back
+//#define fi first
+//#define se second
+//#define vt vector
+//#define ar array
+//#define make_unique(x) sort(all((x))); (x).resize(unique(all((x))) - (x).begin())
+//
+//template<class A> void read(vt<A>& v);
+//template<class A, size_t S> void read(ar<A, S>& a);
+//template<class T> void read(T& x) {
+//    cin >> x;
+//}
+//void read(double& d) {
+//    string t;
+//    read(t);
+//    d=stod(t);
+//}
+//void read(long double& d) {
+//    string t;
+//    read(t);
+//    d=stold(t);
+//}
+//template<class H, class... T> void read(H& h, T&... t) {
+//    read(h);
+//    read(t...);
+//}
+//template<class A> void read(vt<A>& x) {
+//    trav(a, x)
+//        read(a);
+//}
+//template<class A, size_t S> void read(array<A, S>& x) {
+//    trav(a, x)
+//        read(a);
+//}
+//clock_t startTime;
+//double getCurrentTime() {
+//    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+//}
+//// (づ°ω°)づﾐ★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。。・゜・。。・゜☆゜・。。・゜゜・。。・゜゜・。
+////        .==.        .==.
+////       //`^\\      //^`\\
+////      // ^ ^\(\__/)/^ ^^\\
+////     //^ ^^ ^/6  6\ ^^ ^ \\
+////    //^ ^^ ^/( .. )\^ ^ ^ \\
+////   // ^^ ^/\| v""v |/\^ ^ ^\\
+////  // ^^/\/ /  `~~`  \ \/\^ ^\\
+////  -----------------------------
+///*
+//vector<int> countSort(vector<int>& inputArray){
+//    int N = inputArray.size();
+//    // Finding the maximum element of array inputArray[].
+//    int M = 0;
+//    for (int i = 0; i < N; i++)
+//        M = max(M, inputArray[i]);
+//
+//    // Initializing countArray[] with 0
+//    vector<int> countArray(M + 1, 0);
+//
+//    // Mapping each element of inputArray[] as an index
+//    // of countArray[] array
+//
+//    for (int i = 0; i < N; i++)
+//        countArray[inputArray[i]]++;
+//
+//    // Calculating prefix sum at every index
+//    // of array countArray[]
+//    for (int i = 1; i <= M; i++)
+//        countArray[i] += countArray[i - 1];
+//
+//    // Creating outputArray[] from countArray[] array
+//    vector<int> outputArray(N);
+//
+//    for (int i = N - 1; i >= 0; i--){
+//        outputArray[countArray[inputArray[i]] - 1]= inputArray[i];
+//
+//        countArray[inputArray[i]]--;
+//    }
+//
+//    return outputArray;
+//}
+//*/
+//void setIO(string s = ""){
+//    FAST
+//    if(sz(s)){
+//        freopen((s + ".in").c_str(), "r", stdin);
+//        freopen((s + ".out").c_str(), "w", stdout);
+//
+//    }
+//}
+//
+///*
+//ll pw(ll a, ll b) {
+//	ll ans = 1; while (b) {
+//		while (!(b & 1)) b >>= 1, a = (a * a) % MOD;
+//		ans = (ans * a) % MOD, --b;
+//	} return ans;
+//}
+//*/
+//
+///*
+//void bucketsort(int a[], int n){ // function to implement bucket sort
+//    int max = a[0]; // get the maximum element in the array
+//    for (int i = 1; i < n; i++)
+//        if (a[i] > max)
+//            max = a[i];
+//    int b[max], i;
+//    for (int i = 0; i <= max; i++) {
+//        b[i] = 0;
+//    }
+//    for (int i = 0; i < n; i++) {
+//        b[a[i]]++;
+//    }
+//    for (int i = 0, j = 0; i <= max; i++) {
+//        while (b[i] > 0) {
+//            a[j++] = i;
+//            b[i]--;
+//        }
+//    }
+//}
+//*/
+//
+//int gcd(int a, int b) {
+//    while (b != 0) {
+//        int tmp = a % b;
+//        a = b;
+//        b = tmp;
+//    }
+//    return a;
+//}
+//int lcm(int a, int b) {
+//    return a * b / gcd(a, b);
+//}
+//
+//void solve() {
+//
+//    int n;
+//    cin >> n;
+//
+//    vector <int> k(n);
+//    for (int i = 0; i < n; i++) {
+//        cin >> k[i];
+//    }
+//
+//    int z = 1;
+//    for (int i = 0; i < n; i++) {
+//        z = lcm(z, k[i]);
+//    }
+//
+//    int suma = 0;
+//    for (int i = 0; i < n; i++) {
+//        suma += z / k[i];
+//    }
+//
+//    if (suma < z) {
+//        for (int i = 0; i < n; i++) {
+//            cout << z / k[i] << " ";
+//        }
+//        cout << "\n";
+//    } else {
+//        cout << -1 << "\n";
+//    }
+//}
+//
+//
+//
+//void solve() {
+//
+//    int n; read(n);
+//    vi a(n); read(a);
+//
+//    int sub = a[0];
+//    if(sub>a[1]){
+//        cout<<"NO"<<endl;
+//        return;
+//    }
+//    rep(i,1,n-1){
+//        if(a[i]>a[i+1]){
+//            if(a[i] - sub > a[i+1]){
+//                cout<<"NO"<<endl;
+//                return;
+//            }
+//
+//            else{
+//                sub = a[i]-sub;
+//                continue;
+//            }
+//        }
+//        sub = a[i] -sub;
+//
+//    }
+//    cout<<"YES"<<endl;
+//}
+//
+//
+//
+//signed main(){
+//
+//    setIO();
+//
+//    int tc = 1;
+//    cin>>tc;
+//    for(int i = 1; i<=tc; i++){
+//        startTime = clock();
+//        cerr<<"--- Case "<<i<<" start ----"<<endl;
+//        solve();
+//        cerr<<"--- Case "<<i<<" end ----"<<endl;
+//        cerr << setprecision(4) << fixed;
+//        cerr<<"time = "<<getCurrentTime()<<endl;
+//    }
+//
+//
+//    return 0;
+//}
+//
+///*
+//disciple is freedom
+// * do things that you don't feel like doing, the real glory lies over there
+//*/
+///*
+//       - Starting is the hard part
+//            Start reading
+//            Start dissecting, etc, starting these is the hard part, once you've started, it becomes easier
+//       - Finish the sub-problem before jumping to the next
+//       - do things that you don't feel like doing, the real glory lies over there
+//*/
